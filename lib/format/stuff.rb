e6c13916571cbcfd85e3eae3b7851c001c917c19
Christian Köstlin <christian.koestlin@esrlabs.com>
2016-12-11 16:19:57 +0100

    added first gradle spec

--- lib/format/stuff.rb
+++ lib/format/stuff.rb
@@ -4,28 +4,53 @@ module Format
   module Stuff
     def count(l, stuff)
       stuff
-        .map { |s| l.count(s) }
+        .map { |s| l.scan(s).size }
         .reduce(:+)
     end
 
-    def run_format(data, output, indent)
-      open = ['(', '[', '{']
-      close = [')', ']', '}']
+    def count_tmp_indent(l, tmp_indent)
+      (tmp_indent.map{ |i| l.match(i) ? 1 : 0} + [0])
+        .reduce(:+)
+    end
 
+    #def ruby_open
+    #  ["def", "begin", "(", "[", "{"]
+    #end
+    #def ruby_close
+    #  ["end", ")", "]", "}"]
+    #end
+    #def ruby_tmp_indent
+    #  [/^\./, /^\|\|/, /^&&/]
+    #end
+    def gradle_open
+      ["{"]
+    end
+    def gradle_close
+      ["}"]
+    end
+
+    def run_format(data, output, indent, open=['(', '[', '{'], close=[')', ']', '}'], tmp_indent=[])
       indent_level = 0
       data.each_line do |l|
         l.strip!
+        # STDERR.puts "working on '#{l}'"
         opening = count(l, open)
+        # STDERR.puts "opening #{opening}"
         closing = count(l, close)
+        # STDERR.puts "closing #{closing}"
         old_indent_level = indent_level
         indent_level = indent_level + opening - closing
-
         prefix =
           if indent_level > old_indent_level
             indent * old_indent_level
           else
             indent * indent_level
           end
+        tmp_indent_level = count_tmp_indent(l, tmp_indent)
+        # STDERR.puts "tmp_indent #{tmp_indent_level}"
+        # STDERR.puts "prefix '#{prefix}'"
+        prefix = prefix + (indent * tmp_indent_level)
+        # STDERR.puts "prefix '#{prefix}'"
         output.puts((prefix + l).rstrip)
       end
       output.close
--- spec/format/stuff_spec.rb
+++ spec/format/stuff_spec.rb
@@ -1,7 +1,15 @@
 require "spec_helper"
+include Format::Stuff
 
 describe Format::Stuff do
-  include Format::Stuff
+  it "should apply tmp_indents" do
+    expect(count_tmp_indent(".test", [/^\./])).to eq(1)
+  end
+
+  it "should indent 0 if no tmp_indents" do
+    expect(count_tmp_indent(".test", [])).to eq(0)
+  end
+
   it "has a version number" do
     expect(Format::Stuff::VERSION).not_to be nil
   end
@@ -33,5 +41,36 @@ describe Format::Stuff do
     expect(output.string).to eq("test {\n\n}\n")
   end
 
+end
 
+#describe "Format::Stuff::Ruby" do
+#  it "should format ruby files" do
+#    output = StringIO.new
+#    run_format("def test()\ntest()\nend\n", output, "  ", ["def", "begin", "(", "[", "{"], ["end", ")", "]", "}"])
+#    expect(output.string).to eq("def test()\n  test()\nend\n")
+#  end
+#
+#  it "should format chains of method calls nicely" do
+#    output = StringIO.new
+#    run_format("def test()\nhello.there\n[1,2,3]\n.map()\n.map()\nend\n", output, "  ", ruby_open, ruby_close, ruby_tmp_indent)
+#    expect(output.string).to eq("def test()\n  hello.there\n  [1,2,3]\n    .map()\n    .map()\nend\n")
+#  end
+#
+#  it "should be possible to format multiline || nicely" do
+#    output = StringIO.new
+#    run_format("def test()\nif a\n||b\n&&c\nend\n", output, "  ", ruby_open, ruby_close, ruby_tmp_indent)
+#    expect(output.string).to eq("def test()\n  if a\n    ||b\n    &&c\nend\n")
+#  end
+#
+#  it "should format if elsif ends nicely" do
+#    output = StringIO.new
+#  end
+#end
+
+describe "Format::Stuff::Gradle" do
+  it "should format build.gradle files" do
+    output = StringIO.new
+    run_format("apply plugin: 'java'\nrepositories {\njcenter()\n}\n", output, "    ", gradle_open, gradle_close)
+    expect(output.string).to eq("apply plugin: 'java'\nrepositories {\n    jcenter()\n}\n")
+  end
 end
